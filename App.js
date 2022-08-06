fcbcb047f7e4af177fad263b1ecdca926146067b
Muhammad Noman Naeem <59162949+Noman-8560@users.noreply.github.com>
2022-08-06 18:00:47 +0500

    Initial

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Muhammad Noman Naeem</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'dodgerblue',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+});
